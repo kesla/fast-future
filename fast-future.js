449cd13e43c8031f77e3722a7800f97082a1d833
David Björklund <david.bjorklund@gmail.com>
2014-05-19 22:07:30 +0200

    fix bug when running a process.nextTick before starting fastFuture

--- fast-future.js
+++ fast-future.js
@@ -1,7 +1,8 @@
-var factory = function () {
+var LIMIT = process.maxTickDepth / 2
+  , factory = function () {
       var count = 0
       return function (callback) {
-        if (count === process.maxTickDepth){
+        if (count >= LIMIT){
           global.setImmediate(callback)
           count = 0
         } else
